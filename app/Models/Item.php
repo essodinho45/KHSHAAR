5a3b2d9e7fecea6d598d1b8ffa3c115b7bba3a0a
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2024-10-26 12:45:14 +0300

    make migrations, models and controllers

---
+++ app/Models/Item.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Item extends Model
+{
+    use SoftDeletes;
+    protected $fillable = [
+        'name',
+        'description',
+        'image_url',
+        'brand_id',
+    ];
+    /**
+     * Get the user that owns the Item
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+}
