5ea418efc1cdbd7176fb93f55c5adf8d98d86706
Essam Dimashky <essam.dimashky@tatweer.sy>
2024-12-25 22:23:59 +0300

    brands

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,7 +61,7 @@
                 <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
                     <a href="#">
                         <div class="h-full flex items-center border-gray-800 border p-4 rounded-lg">
-                            <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="storage/{{$brand->logo_url}}">
+                            <img alt="logo" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="storage/{{$brand->logo_url}}">
                             <div class="flex-grow">
                                 <h2 class="text-white title-font font-medium">{{$brand->name}}</h2>
                             </div>
