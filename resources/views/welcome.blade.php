f834d712933ad3901b15acaff0192feb8df9bac6
Essam Dimashky <essam.dimashky@tatweer.sy>
2024-12-25 22:22:25 +0300

    brands

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,45 +50,23 @@
         </div>
     </div>
 </section>
-<section class="text-gray-600 body-font">
-    <div class="container flex flex-wrap px-5 py-24 mx-auto items-center">
-        <div class="md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-gray-200">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
-            <p class="leading-relaxed text-base">Locavore cardigan small batch roof party blue bottle blog meggings sartorial jean shorts kickstarter migas sriracha church-key synth succulents. Actually taiyaki neutra, distillery gastropub pok pok ugh.</p>
-            <a class="text-indigo-500 inline-flex items-center mt-4">Learn More
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-            </a>
+<section class="text-gray-400 bg-gray-900 body-font">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-col text-center w-full mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Brands</h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Wide collection of top electrical brands.</p>
         </div>
-        <div class="flex flex-col md:w-1/2 md:pl-12">
-            <h2 class="title-font font-semibold text-gray-800 tracking-wider text-sm mb-3">CATEGORIES</h2>
-            <nav class="flex flex-wrap list-none -mb-1">
-                <li class="lg:w-1/3 mb-1 w-1/2">
-                    <a class="text-gray-600 hover:text-gray-800">First Link</a>
-                </li>
-                <li class="lg:w-1/3 mb-1 w-1/2">
-                    <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-                </li>
-                <li class="lg:w-1/3 mb-1 w-1/2">
-                    <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-                </li>
-                <li class="lg:w-1/3 mb-1 w-1/2">
-                    <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                </li>
-                <li class="lg:w-1/3 mb-1 w-1/2">
-                    <a class="text-gray-600 hover:text-gray-800">Fifth Link</a>
-                </li>
-                <li class="lg:w-1/3 mb-1 w-1/2">
-                    <a class="text-gray-600 hover:text-gray-800">Sixth Link</a>
-                </li>
-                <li class="lg:w-1/3 mb-1 w-1/2">
-                    <a class="text-gray-600 hover:text-gray-800">Seventh Link</a>
-                </li>
-                <li class="lg:w-1/3 mb-1 w-1/2">
-                    <a class="text-gray-600 hover:text-gray-800">Eighth Link</a>
-                </li>
-            </nav>
+        <div class="flex flex-wrap -m-2">
+            @foreach($brands as $brand)
+                <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                    <div class="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+                        <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="storage/{{$brand->logo_url}}">
+                        <div class="flex-grow">
+                            <h2 class="text-white title-font font-medium">{{$brand->name}}</h2>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </section>
